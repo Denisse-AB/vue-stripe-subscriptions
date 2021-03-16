4dce3cdccb0b7e239d2363cc715c1eb03b84f91e
Denisse Abreu <denisseabreu21@yahoo.com>
2021-03-16 15:07:16 -0400

    thankyou page

---
+++ client/src/views/ThankYou.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+      <h1 class="text-center">Thankyou page!</h1>
+    </v-container>
+</template>
+
+<script>
+/* eslint linebreak-style: ["error", "windows"] */
+export default {
+  name: 'ThankYou',
+
+  mounted() {
+    // mount element if transaction ok.
+  },
+};
+</script>
