516c2ae538312816646fff4ff4e8b2afcc045258
Denisse Abreu <denisseabreu21@yahoo.com>
2021-03-16 15:08:02 -0400

    vue router config

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/thankyou',
+    name: 'ThankYou',
+    component: () => import('../views/ThankYou.vue'),
+  },
 ];
 
 const router = new VueRouter({
